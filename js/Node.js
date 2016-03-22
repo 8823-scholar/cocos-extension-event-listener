1b7468c9d40fa6fc33cbe9e945c0108fbed9eba1
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2016-03-22 17:29:12 +0900

    event blocking by instances.

--- js/Node.js
+++ js/Node.js
@@ -25,7 +25,26 @@ cc.Node.implement({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
             swallowsTouches: true,
             onTouchBegan: function(touch, event) {
-                return self.hitTestByPoint(touch.getLocation());
+
+                var blocked = false;
+                var parents = self.getParants();
+                loop: {
+                    for (var i = 0; i < parents.length; i++) {
+                        var parent = parents[i];
+
+                        for (var j = parent.children.length; j > 0; j--) {
+                            var child = parent.children[j - 1];
+                            if (child === parent.child) break;
+
+                            if (child.hitTestByPoint(touch.getLocation())) {
+                                blocked = true;
+                                break loop;
+                            }
+                        }
+                    }
+                }
+
+                return !blocked && self.hitTestByPoint(touch.getLocation());
             },
             onTouchMoved: null,
             onTouchEnded: function(touch, event) {
@@ -58,6 +77,8 @@ cc.Node.implement({
      * get node content size
      */
     getBoundingBox: function() {
+        if (! this.isVisible() || ! this.opacity) return new cc.Rect(0, 0, 0, 0);
+
         if (this._contentSize && (this._contentSize.width || this._contentSize.height)) {
             var rect = cc.rect(0, 0, this._contentSize.width, this._contentSize.height);
             return cc._rectApplyAffineTransformIn(rect, this.getNodeToParentTransform());
@@ -88,17 +109,44 @@ cc.Node.implement({
         if (minY === null) minY = 0;
 
         var rect = cc.rect(minX, minY, maxX - minX, maxY - minY);
-        return cc._rectApplyAffineTransformIn(rect, this.getNodeToParentTransform());
+        return rect;
     },
 
 
     /**
      * hit test
+     *
+     * @param   cc.Point    point in global
      */
-    hitTestByPoint: function(point) {
-        var size = this.getBoundingBox();
-        var rect = cc.rect(size.x, size.y, size.width, size.height);
-        return cc.rectContainsPoint(rect, this.convertToNodeSpace(point));
+    hitTestByPoint: function(point)
+    {
+        if (this instanceof cc.Layer || this instanceof cc.Scene || this._className == "Node") {
+            if (this.isVisible() && this.opacity) {
+                for (var i = 0; i < this.children.length; i++) {
+                    var child = this.children[i];
+                    if (child.hitTestByPoint(point)) return true;
+                }
+            }
+            return false;
+        } else {
+            var rect = this.getBoundingBox();
+            return cc.rectContainsPoint(rect, this.getParent().convertToNodeSpace(point));
+        }
+    },
+
+
+    /**
+     * get all parents recursive
+     */
+    getParants: function()
+    {
+        var parent = null, self = this, parents = [];
+        while (parent = self.getParent()) {
+            parents.push(parent);
+            parent.child = self;
+            self = parent;
+        }
+        return parents.reverse();
     },
 });
 
